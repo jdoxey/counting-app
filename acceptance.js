c157177afdad0bde639cbcfe448a47181971a0c9
John Doxey <john.doxey@gmail.com>
2014-03-18 23:41:56 +1100

    Added UIAutomation script

---
+++ acceptance.js
@@ -0,0 +1,8 @@
+
+var target = UIATarget.localTarget();
+
+target.frontMostApp().mainWindow().buttons()["Ten"].tap();
+target.frontMostApp().mainWindow().buttons()["Twenty"].tap();
+target.frontMostApp().mainWindow().buttons()["Ten"].tap();
+target.frontMostApp().mainWindow().buttons()["Twenty"].tap();
+
